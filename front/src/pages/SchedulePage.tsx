d2dde402b5607dc060ef49c7c64913cf08122f54
MIMBOL228 <aekemerov@yandex.ru>
2023-10-14 22:14:36 +0500

    Начало classRange в Front

--- front/src/pages/SchedulePage.tsx
+++ front/src/pages/SchedulePage.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "../css/schedule.css";
 
@@ -6,23 +6,35 @@ import DaySchedule from "../сomponents/DaySchedule/DaySchedule";
 import NavbarComponent from "../сomponents/Navbar/Navbar";
 import { useWeekDates } from "./hooks/useWeekDates";
 import { useUserInfo } from "../hooks/useUserInfo";
+import { IClassRangeState } from "../interfaces/IClassRange";
+import { ClassRangeItem } from "../сomponents/ClassRangeItem/ClassRangeItem";
 
 export default function SchedulePage() {
-    //fixme выглядит, как костыль
-    const [days] = useState([[], [], [], [], [], [], []]);
-    const weekDates = useWeekDates();
-    const userInfo = useUserInfo();
+	//fixme выглядит, как костыль
+	const [days] = useState([[], [], [], [], [], [], []]);
 
-    return (
-        <>
-            <NavbarComponent
-                userInfo={userInfo}
-            />
-            <div className="days_wrapper" style={{overflow: "auto"}}>
-                {days.map(
-                    (e, k) =><DaySchedule key={k} dow={k} weekDates={weekDates} />
-                )}
-            </div>
-        </>
-    );
+	const [classRanges, setClassRanges] = useState<IClassRangeState[]>([]);
+
+	const weekDates = useWeekDates();
+	const userInfo = useUserInfo();
+
+	return (
+		<>
+			<NavbarComponent
+				userInfo={userInfo}
+			/>
+			<div className="class_range_wrapper">
+				{
+					classRanges.map(
+						(e, k) => <ClassRangeItem />
+					)
+				}
+			</div>
+			<div className="days_wrapper" style={{ overflow: "auto" }}>
+				{days.map(
+					(e, k) => <DaySchedule key={k} dow={k} weekDates={weekDates} />
+				)}
+			</div>
+		</>
+	);
 }
---
+++ front/src/сomponents/ClassRangeItem/ClassRangeItem.tsx
@@ -0,0 +1,9 @@
+import "./ClassRangeItem.css";
+
+export const ClassRangeItem = () => {
+  return (
+    <div className="class_range">
+      <p></p>
+    </div>
+  );
+};
