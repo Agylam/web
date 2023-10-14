5ac7cbad4c942fcd8950dbd04030a6952495dbd6
MIMBOL228 <aekemerov@yandex.ru>
2023-10-14 22:37:11 +0500

    Сохранение сессии в LS

--- front/src/context/jwt-context.tsx
+++ front/src/context/jwt-context.tsx
@@ -1,42 +1,32 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { loadJwts, saveJwts } from "../utils/jwts";
+import { createContext, useContext } from "react";
+import useLocalStorage from "use-local-storage";
 
 export interface JWTs {
-  accessToken: string;
-  refreshToken: string;
+    accessToken: string;
+    refreshToken: string;
 }
 
 type JwtContextType = {
-  jwts: JWTs;
-  setJwts: (jwts: JWTs) => void;
-}
+    jwts: JWTs;
+    setJwts: (jwts: JWTs) => void;
+};
 
 const defaultValue = {
-  jwts: {
-    accessToken: '',
-    refreshToken: '',
-  },
-  setJwts: () => {},
+    jwts: {
+        accessToken: "",
+        refreshToken: "",
+    },
+    setJwts: () => {},
 };
 
 const JwtContext = createContext<JwtContextType>(defaultValue);
 
 export const useJwtContext = () => {
-  const context = useContext(JwtContext);
-  return context;
+    const context = useContext(JwtContext);
+    return context;
 };
 
 export const JwtProvider = (props: Record<any, any>) => {
-  const [jwts, setJwts] = useState<JWTs>(defaultValue.jwts);
-  useEffect(() => {
-    try {
-      const loadedJwts = loadJwts();
-      loadedJwts && setJwts(loadedJwts);
-    } catch (err) {
-      console.error(err);
-    }
-  }, []);
-
-  useEffect(() => saveJwts(jwts), [jwts]);
-  return <JwtContext.Provider value={{ jwts, setJwts }} {...props} />;
+    const [jwts, setJwts] = useLocalStorage<JWTs>("jwt", defaultValue.jwts);
+    return <JwtContext.Provider value={{ jwts, setJwts }} {...props} />;
 };
