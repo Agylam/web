bcf7e46eddb179951cc20949281a5145040ecc70
MIMBOL228 <aekemerov@yandex.ru>
2023-10-22 22:22:55 +0500

    Зачатки backend обновления расписания

--- back/src/schedule/schedule.controller.ts
+++ back/src/schedule/schedule.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Put, UseGuards } from '@nestjs/common';
 import { ScheduleService } from './schedule.service.js';
 import { Roles } from '../auth/roles-auth.decorator.js';
 import { RolesGuard } from '../auth/roles.guard.js';
@@ -11,6 +11,13 @@ export class ScheduleController {
     @UseGuards(RolesGuard)
     @Get('/:class_range_uuid/:day')
     async get(@Param('class_range_uuid') class_range_uuid: string, @Param('day') day: number) {
+        return this.scheduleService.getSchedule(class_range_uuid, day);
+    }
+
+    @Roles('headteacher')
+    @UseGuards(RolesGuard)
+    @Put('/:class_range_uuid/:day')
+    async put(@Param('class_range_uuid') class_range_uuid: string, @Param('day') day: number) {
         return this.scheduleService.getSchedule(class_range_uuid, day);
     }
 }
