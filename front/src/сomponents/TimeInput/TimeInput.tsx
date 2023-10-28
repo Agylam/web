96d5ae69c8cb09c5500cbcfa23698c4d80a54794
MIMBOL228 <aekemerov@yandex.ru>
2023-10-28 23:08:51 +0500

    3+3=9

--- front/src/сomponents/TimeInput/TimeInput.tsx
+++ front/src/сomponents/TimeInput/TimeInput.tsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./TimeInput.css";
 
 interface TimeInputComponentParams {
-    value: string;
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+	value: string;
+	onUpdateValue: (event: string) => void;
 }
 
-export default function TimeInput({value, onChange}: TimeInputComponentParams) {
-    return (
-        <input
-            type="time"
-            className="time_input"
-            value={value}
-            onChange={onChange}
-        />
-    );
+export default function TimeInput({ value, onUpdateValue }: TimeInputComponentParams) {
+	const [timeValue, setTimeValue] = useState("10:00");
+
+	useEffect(() => {
+		setTimeValue(value);
+	}, [value, setTimeValue]);
+
+	return (
+		<input
+			type="time"
+			className="time_input"
+			value={timeValue}
+			onChange={(e) => setTimeValue(e.target.value)}
+			onBlur={(e) => {
+				console.log("UnFocus");
+				onUpdateValue(timeValue);
+			}}
+		/>
+	);
 }
--- front/src/сomponents/TimeRange/TimeRange.tsx
+++ front/src/сomponents/TimeRange/TimeRange.tsx
@@ -5,23 +5,23 @@ import TimeInput from "../TimeInput/TimeInput";
 import "./TimeRange.css";
 
 interface TimeRangeComponentParams {
-    timeRange: {
-        start: string;
-        end: string;
-    };
-    changeTime: (type: boolean, time: string) => void
+	timeRange: {
+		start: string;
+		end: string;
+	};
+	changeTime: (type: boolean, time: string) => void;
 }
 
-export default function TimeRangeComponent({timeRange, changeTime}: TimeRangeComponentParams) {
-    return (
-        <div className="time_range">
-            <TimeInput
-                value={timeRange.start}
-                onChange={(event) => changeTime(false, event.target.value)}/>
-            <p className="time_range_wall">:</p>
-            <TimeInput
-                value={timeRange.end}
-                onChange={(event) => changeTime(true, event.target.value)}/>
-        </div>
-    );
+export default function TimeRangeComponent({ timeRange, changeTime }: TimeRangeComponentParams) {
+	return (
+		<div className="time_range">
+			<TimeInput
+				value={timeRange.start}
+				onUpdateValue={(value) => changeTime(false, value)} />
+			<p className="time_range_wall">:</p>
+			<TimeInput
+				value={timeRange.end}
+				onUpdateValue={(value) => changeTime(true, value)} />
+		</div>
+	);
 }
