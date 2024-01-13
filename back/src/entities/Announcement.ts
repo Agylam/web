407afd981bc4437eeb5da7e36aefee964c2a2a89
MIMBOL228 <aekemerov@yandex.ru>
2024-01-14 04:18:11 +0500

    Создад Announcement Entity

---
+++ back/src/entities/Announcement.ts
@@ -0,0 +1,29 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { School } from './School.js';
+
+export enum AnnouncementState {
+    CREATED,
+    PUSHED,
+    PLAYING,
+    PLAYED,
+    ERROR,
+}
+
+@Entity()
+export class Announcement extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    uuid: string;
+
+    @Column({ length: 2000 })
+    text: string;
+
+    @Column({ type: 'enum', enum: AnnouncementState, default: AnnouncementState.CREATED })
+    state: AnnouncementState;
+
+    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)' })
+    created_at: Date;
+
+    @ManyToOne(() => School, (school) => school.announcements)
+    @JoinColumn()
+    school: School;
+}
